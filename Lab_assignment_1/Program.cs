84707ee790d5ac3977c86085e40d168669d21062
SreejaYathipathi <sreejayathipathi@gmail.com>
2024-09-09 13:29:21 -0700

    Lab assignment 1
    Basic project for decimal, hexadecimal and binary

---
+++ Lab_assignment_1/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Lab_assignment_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hey everyone, It's interesting to note these numbers in decimal form: ");
+            Console.WriteLine("0, 1, 2, 4, 8, 16, 32, 64, 100, 255");
+            Console.WriteLine("can be converted into their binary equivalents: ");
+            Console.WriteLine("0, 1, 10, 100, 1000, 10000, 100000, 1000000, 1100100, 11111111");
+            Console.WriteLine("and their hexadecimal equivalents are: ");
+            Console.WriteLine("0, 1, 2, 4, 8, 10, 20, 40, 64, FF");
+            Console.WriteLine("Thank you for taking the time to read.");
+            Console.WriteLine("Sreeja Yathipathi, 09/09/2024");
+        }
+    }
+}

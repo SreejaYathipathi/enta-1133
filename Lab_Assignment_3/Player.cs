23e32765b9b41db1accebc205d5906c06e7bab87
SreejaYathipathi <sreejayathipathi@gmail.com>
2024-09-27 11:11:07 -0700

    Dice game for Player and Computer
    Created a dice game where player and computer selects a random dice and rolls it.
    
    - A used dice can't be selected twice.
    - Whoever scores the highest number, WINS.

---
+++ Lab_Assignment_3/Player.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_Assignment_3
+{
+    internal class Player
+    {
+        internal int score = 0; // Shared score value for player and computer
+    }
+}
